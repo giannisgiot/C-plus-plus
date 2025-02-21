e0414081137d73d93ecf57eefb8e2c5ca855d8e0
giannisgiot <g1ann1sg1otak1s@gmail.com>
2025-02-21 17:57:07 +0200

    Create ask_cin_cout.cpp

---
+++ Lesson1/ask_cin_cout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int i;
+    double j;
+
+    cout << "Enter number: ";
+    cin >> i; // Διάβασμα ακεραίου και αποθήκευσή του στο i.
+    cout << "Enter number: ";
+    cin >> j;
+    cout << i << '\t' << fixed << j << '\n';
+    return 0;
+}

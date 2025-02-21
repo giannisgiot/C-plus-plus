95889fe37a697c512ca8cae5727b6df8b56fe686
giannisgiot <g1ann1sg1otak1s@gmail.com>
2025-02-21 18:01:23 +0200

    Create Ask_sizeOf.cpp

---
+++ Lesson1/Ask_sizeOf.cpp
@@ -0,0 +1,17 @@
+// #include <bits/stdc++.h>
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    float b;
+    char g;
+
+    // Printing size of all three variables
+    cout << "Size of a:  " << sizeof(a) << endl;
+    cout << "Size of b:  " << sizeof(b) << endl;
+    cout << "Size of g:  " << sizeof(g);
+
+    return 0;
+}
